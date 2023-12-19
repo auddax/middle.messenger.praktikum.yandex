b88236aa2ae1da66c5eede6a3d3e0e7dc7d1f922
Kirill Konovalov <auddax@gmail.com>
2023-12-19 20:25:16 +0300

    build: add handlebars and sass packages to the project (Tue, Dec 19, 2023 08:25:16 PM)

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite'
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-  plugins: [handlebars()],
+  plugins: [
+    handlebars()
+  ],
+  build: {
+    outDir: 'build'
+  }
 });
