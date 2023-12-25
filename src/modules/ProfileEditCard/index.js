87322f14eb1d8d2a0badb0920b7d84f533ca1008
Kirill Konovalov <auddax@gmail.com>
2023-12-25 21:26:17 +0300

    fix: add id attributes for all inputs (Mon, Dec 25, 2023 09:26:17 PM) (Mon, Dec 25, 2023 09:34:08 PM)

--- src/modules/ProfileEditCard/index.js
+++ src/modules/ProfileEditCard/index.js
@@ -4,21 +4,21 @@ export { default as ProfileEditCard } from './ProfileEditCard.hbs?raw';
 
 Handlebars.registerHelper('profileEditItems', () => [
   {
-    label: 'Почта', htmlType: 'email', name: 'email',
+    label: 'Почта', htmlType: 'email', name: 'email', id: 'profileEmailInput',
   },
   {
-    label: 'Логин', htmlType: 'text', name: 'login',
+    label: 'Логин', htmlType: 'text', name: 'login', id: 'profileLoginInput',
   },
   {
-    label: 'Имя', htmlType: 'text', name: 'first_name',
+    label: 'Имя', htmlType: 'text', name: 'first_name', id: 'profileFirsNameInput',
   },
   {
-    label: 'Фамилия', htmlType: 'text', name: 'second_name',
+    label: 'Фамилия', htmlType: 'text', name: 'second_name', id: 'profileSecondNameInput',
   },
   {
-    label: 'Имя в чате', htmlType: 'text', name: 'display_name',
+    label: 'Имя в чате', htmlType: 'text', name: 'display_name', id: 'profileDisplayNameInput',
   },
   {
-    label: 'Телефон', htmlType: 'number', name: 'phone',
+    label: 'Телефон', htmlType: 'number', name: 'phone', id: 'profilePhoneInput',
   },
 ]);
