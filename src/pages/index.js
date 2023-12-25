629446fbd9adc9d6571337812abafec64f9f8736
Kirill Konovalov <auddax@gmail.com>
2023-12-25 13:12:51 +0300

    feat: set up routing (Mon, Dec 25, 2023 01:12:51 PM) (Mon, Dec 25, 2023 02:47:06 PM)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 export { ChatPage } from './ChatPage';
 export { LoginPage } from './LoginPage';
 export { ProfilePage } from './ProfilePage';
+export { ProfileEditPage } from './ProfileEditPage';
+export { ProfileEditPasswordPage } from './ProfileEditPasswordPage';
 export { SignupPage } from './SignupPage';
 export { NotFoundPage } from './NotFoundPage';
 export { ErrorPage } from './ErrorPage';
