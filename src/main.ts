7e6d88958f6c8775f1f6943edc9f2068c6da9046
Kirill Konovalov <auddax@gmail.com>
2024-01-09 18:53:27 +0300

    chore: add typescript to the project (Tue, Jan 09, 2024 06:53:27 PM) (Tue, Jan 16, 2024 08:40:45 PM)

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,15 @@ import * as Modules from './modules';
 import * as Layout from './layout';
 import * as Pages from './pages';
 
-const pages = {
-  loginPage: [Pages.LoginPage],
-  signupPage: [Pages.SignupPage],
-  errorPage: [Pages.ErrorPage],
-  notFoundPage: [Pages.NotFoundPage],
-  chatPage: [Pages.ChatPage],
-  profilePage: [Pages.ProfilePage],
-  profileEditPage: [Pages.ProfileEditPage],
-  profileEditPasswordPage: [Pages.ProfileEditPasswordPage],
+const pages: { [key: string]: string } = {
+  loginPage: Pages.LoginPage,
+  signupPage: Pages.SignupPage,
+  errorPage: Pages.ErrorPage,
+  notFoundPage: Pages.NotFoundPage,
+  chatPage: Pages.ChatPage,
+  profilePage: Pages.ProfilePage,
+  profileEditPage: Pages.ProfileEditPage,
+  profileEditPasswordPage: Pages.ProfileEditPasswordPage,
 };
 
 Object.entries(Components).forEach(([name, component]) => {
@@ -27,15 +27,16 @@ Object.entries(Layout).forEach(([name, component]) => {
   Handlebars.registerPartial(name, component);
 });
 
-const navigate = (page) => {
-  const [source, context] = pages[page];
+const navigate = (pageName: string): void => {
+  const source = pages[pageName];
   const container = document.getElementById('app');
-  container.innerHTML = Handlebars.compile(source)(context);
+  if (container) container.innerHTML = Handlebars.compile(source)({});
 };
 
 document.addEventListener('DOMContentLoaded', () => navigate('loginPage'));
 document.addEventListener('click', (e) => {
-  const page = e.target.getAttribute('page');
+  const target = e.target as HTMLElement;
+  const page = target.getAttribute('page');
   if (page) {
     navigate(page);
     e.preventDefault();
