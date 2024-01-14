2abbc8f8682d3af73847d4d2a3c5d7232f192ec0
Kirill Konovalov <auddax@gmail.com>
2024-01-14 19:48:04 +0300

    feat: add navigate function (Sun, Jan 14, 2024 07:48:04 PM) (Tue, Jan 16, 2024 08:40:46 PM)

---
+++ src/utils/navigate.ts
@@ -0,0 +1,20 @@
+import * as Pages from 'src/pages'
+import Block from 'src/core/Block';
+
+const pages: { [key: string]: typeof Block } = {
+  loginPage: Pages.LoginPage,
+  signupPage: Pages.SignupPage,
+  errorPage: Pages.ErrorPage,
+  notFoundPage: Pages.NotFoundPage,
+  chatPage: Pages.ChatPage,
+  profilePage: Pages.ProfilePage,
+  profileEditPage: Pages.ProfileEditPage,
+  profileEditPasswordPage: Pages.ProfileEditPasswordPage,
+};
+
+export function navigate(pageName: string) {
+  const Page = pages[pageName];
+  const page = new Page();
+  const container = document.getElementById('app');
+  container?.append(page.render());
+};
