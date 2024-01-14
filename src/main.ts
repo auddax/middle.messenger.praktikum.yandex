7de163ccd9a0052438a7fdb39333b074a8b90dcb
Kirill Konovalov <auddax@gmail.com>
2024-01-14 20:02:07 +0300

    feat: set up components imports and registration (Sun, Jan 14, 2024 08:02:07 PM)

--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,20 @@
-import Handlebars from 'handlebars';
 import * as Components from './components';
-import * as Modules from './modules';
 import * as Layout from './layout';
-import * as Pages from './pages';
-
-const pages: { [key: string]: string } = {
-  loginPage: Pages.LoginPage,
-  signupPage: Pages.SignupPage,
-  errorPage: Pages.ErrorPage,
-  notFoundPage: Pages.NotFoundPage,
-  chatPage: Pages.ChatPage,
-  profilePage: Pages.ProfilePage,
-  profileEditPage: Pages.ProfileEditPage,
-  profileEditPasswordPage: Pages.ProfileEditPasswordPage,
-};
+import * as Modules from './modules';
+import { registerComponent } from 'src/utils/registerComponent';
+import { navigate } from 'src/utils/navigate';
 
 Object.entries(Components).forEach(([name, component]) => {
-  Handlebars.registerPartial(name, component);
-});
-
-Object.entries(Modules).forEach(([name, component]) => {
-  Handlebars.registerPartial(name, component);
+  registerComponent(name, component);
 });
 
 Object.entries(Layout).forEach(([name, component]) => {
-  Handlebars.registerPartial(name, component);
+  registerComponent(name, component);
 });
 
-const navigate = (pageName: string): void => {
-  const source = pages[pageName];
-  const container = document.getElementById('app');
-  if (container) container.innerHTML = Handlebars.compile(source)({});
-};
+Object.entries(Modules).forEach(([name, component]) => {
+  registerComponent(name, component);
+});
 
 document.addEventListener('DOMContentLoaded', () => navigate('loginPage'));
 document.addEventListener('click', (e) => {
