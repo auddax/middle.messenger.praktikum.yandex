d8b21771b306990754a0dbf346d947eba1000c8f
Kirill Konovalov <auddax@gmail.com>
2024-02-06 21:59:55 +0300

    feat: add Route core class (Tue, Feb 06, 2024 09:59:55 PM)

---
+++ src/core/Route.ts
@@ -0,0 +1,44 @@
+import { isEqual } from 'src/utils/isEqual';
+import Block from 'src/core/Block';
+import { Props } from 'src/types';
+
+class Route {
+  private _pathname: string;
+
+  private readonly _blockClass: typeof Block;
+
+  private _block: Block | null = null;
+
+  private readonly _props: Props;
+
+  constructor(pathname: string, view: typeof Block, props: Props) {
+    this._pathname = pathname;
+    this._blockClass = view;
+    this._block = null;
+    this._props = props;
+  }
+
+  navigate(pathname: string) {
+    if (pathname === this._pathname) {
+      this._pathname = pathname;
+      this.render();
+    }
+  }
+
+  match(pathname: string) {
+    return isEqual(pathname, this._pathname);
+  }
+
+  render() {
+    this._block = new this._blockClass();
+    if (!this._props.rootQuery) return;
+    const container = document.getElementById(this._props.rootQuery as string);
+    if (container?.firstElementChild) {
+      container?.firstElementChild?.replaceWith(this._block.render());
+    } else {
+      container?.append(this._block.render());
+    }
+  }
+}
+
+export default Route;
