524ec4de101aed213ff36611b17c5f49feefdacc
Kirill Konovalov <auddax@gmail.com>
2024-02-11 18:33:25 +0300

    feat: add ChatAddModal module (Sun, Feb 11, 2024 06:33:25 PM) (Fri, Feb 16, 2024 05:20:49 PM)

---
+++ src/modules/ChatAddModal/ChatAddModal.ts
@@ -0,0 +1,31 @@
+import Block from 'src/core/Block';
+import { focusOutHandler, getFormData } from 'src/utils/handlers';
+import { createChat } from 'src/services/chat';
+import template from './ChatAddModal.hbs?raw';
+
+class ChatAddModal extends Block {
+  constructor() {
+    super({
+      focusOutHandler,
+      handleCreateChat: async () => {
+        const formProps = getFormData('chatNameForm');
+        if (formProps) {
+          const title = formProps.chat_name as string;
+          const resp = await createChat(title);
+          if (resp) this.remove();
+        }
+      },
+    });
+  }
+
+  remove() {
+    const modal = document.querySelector('.backdrop');
+    if (modal) modal.remove();
+  }
+
+  render() {
+    return this.compile(template, this.props);
+  }
+}
+
+export { ChatAddModal };
